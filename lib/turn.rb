5c303fc562fccc3196c1ee0b7c6e388d09e37cef
Lung Chen <lungheichen@gmail.com>
2018-08-04 14:18:57 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,3 +11,13 @@ end
 def move(board, index, token="X")
   board[index-1] = token
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets
+  if (input >= 1) && (input <= 9)
+    input
+  else
+    puts "Invalid entry! Try again..."
+    input = turn(board)
+end
